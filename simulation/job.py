b8f1c98efdfd3776b6c18ce18d11798baa5d0ee7
tarao1006 <taiga4562@gmail.com>
2019-12-12 09:53:04 +0900

    Add scan_dir function

--- simulation/job.py
+++ simulation/job.py
@@ -4,8 +4,11 @@
 移動しながらkapselを実行する。
 """
 import glob
+import pickle
 import subprocess
 from pathlib import Path
+from collections.abc import Sequence
+from functools import partial
 
 
 def get_lines(cmd, cwd):
@@ -31,11 +34,13 @@ def get_lines(cmd, cwd):
             break
 
 
-def scan_simulation_dir(path_dir=None) -> list:
+def scan_dir(contents, path_dir=None) -> list:
     """
 
     Parameters
     ----------
+    contents: str or list of str
+        File list to look for. This must have more than 2.
     path_dir: str, Path or None
 
     Returns
@@ -45,6 +50,12 @@ def scan_simulation_dir(path_dir=None) -> list:
         If path_dir is invalid or there is no directories which
         have both of input.udf and defin.udf, return None.
     """
+    if not isinstance(contents, Sequence):
+        raise TypeError(f"contents have to be sequence type, but '{contents}' is {type(contents)}")
+    if len(contents) < 2:
+        raise TypeError(f"contents have to more than 2 elements, "
+                        f"but {contents} have {len(contents)} elemtent.")
+
     if path_dir is None:
         path_dir = Path('.')
     if not (isinstance(path_dir, str) or isinstance(path_dir, Path)):
@@ -52,11 +63,29 @@ def scan_simulation_dir(path_dir=None) -> list:
 
     path_dir = Path(path_dir)
 
-    input_parents = set(Path(filename).parent for filename in glob.glob(f'{str(path_dir)}/**/input.udf', recursive=True))
-    define_parents = set(Path(filename).parent for filename in glob.glob(f'{str(path_dir)}/**/define.udf', recursive=True))
+    file_sets = []
+    for content in contents:
+        file_sets.append(set(Path(filename).parent for filename in glob.glob(f'{str(path_dir)}/**/{content}', recursive=True)))
 
-    simulation_dirs = input_parents & define_parents
-    if simulation_dirs == set():
+    dirs = file_sets[0] & file_sets[1]
+    if len(file_sets) >= 3:
+        for file_set in file_sets[2:]:
+            dirs = dirs & file_set
+
+    if dirs == set():
         return None
 
-    return simulation_dirs
+    return dirs
+
+
+scan_kapsel_simulation_dirs = partial(scan_dir, contents=['input.udf', 'define.udf'])
+
+
+if __name__ == "__main__":
+    simulation_dirs = scan_kapsel_simulation_dirs(path_dir='.')
+    with open('dirs.pickle', 'wb') as f:
+        pickle.dump(simulation_dirs, f)
+    with open('dirs.pickle', 'rb') as f:
+        dirs = pickle.load(f)
+
+    print(dirs)
