5a36ed88fab5d0bb7a87274593b2bf1586c0f8c6
tarao1006 <taiga4562@gmail.com>
2019-12-13 12:23:00 +0900

    Add argparse to select pickle file

--- simulation/confirm.py
+++ simulation/confirm.py
@@ -1,12 +1,25 @@
-#!/home/opt/intel/intelpython3/bin/python
 import pickle
+import argparse
+from pathlib import Path
 from pprint import pprint
 
 
 if __name__ == "__main__":
-    with open('dirs.pickle', 'rb') as f:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", help="pickle file name", type=str, default="dirs.pickle")
+    args = parser.parse_args()
+    pickle_filename = Path(args.filename)
+
+    with pickle_filename.open(mode='rb') as f:
         dirs = pickle.load(f)
-    
-    print(f'left : {len(dirs)}')    
+
+    left_num = len(dirs)
+    print(f'left : {left_num}')
     for directory in dirs:
         pprint(str(directory))
+
+    if left_num == 0:
+        print(f"Remove {pickle_filename}? (y/n)")
+        reply = input()
+        if reply.upper() == 'Y' or reply.upper() == 'YES':
+            pickle_filename.unlink()
