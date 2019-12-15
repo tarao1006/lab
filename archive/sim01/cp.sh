926447bdb569892e736bb57a1d3ef8b58ec22702
tarao1006 <taiga4562@gmail.com>
2019-12-15 20:35:23 +0900

    Change simulation number

--- archive/sim01/cp.sh
+++ archive/sim01/cp.sh
@@ -3,8 +3,8 @@ for i in `seq 0 10 360`
 do 
 	j=`printf %03d $i`
 	mkdir ./udf/$j
-	cp /home/katarao/Projects/dontuse/strage/sim00/udf/004/$j/output.udf ./udf/$j
-	cp /home/katarao/Projects/dontuse/strage/sim00/udf/004/$j/input.udf ./udf/$j
-	cp /home/katarao/Projects/dontuse/strage/sim00/udf/004/$j/define.udf ./udf/$j
-	cp /home/katarao/Projects/dontuse/strage/sim00/stdout/004/$j/output.txt ./udf/$j
+	cp /home/katarao/Projects/dontuse/strage/sim00/udf/002/$j/output.udf ./udf/$j
+	cp /home/katarao/Projects/dontuse/strage/sim00/udf/002/$j/input.udf ./udf/$j
+	cp /home/katarao/Projects/dontuse/strage/sim00/udf/002/$j/define.udf ./udf/$j
+	cp /home/katarao/Projects/dontuse/strage/sim00/stdout/002/$j/output.txt ./udf/$j
 done
