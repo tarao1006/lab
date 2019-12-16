d0533130663eaec2545d81bf2b6942c6a6386eb5
tarao1006 <taiga4562@gmail.com>
2019-12-16 14:35:14 +0900

    Move gather.sh

--- simulation/sim01/gather.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in `seq 1 32`
-do
-	j=`printf %02d $i`
-	tail -n 10 udf/$j/output.txt | head -n 1 >> test.txt
-done
--- simulation/sim08/gather.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in `seq 0 10 360`
-do
-	j=`printf %03d $i`
-	tail -n 10 udf/$j/output.txt | head -n 1 >> test.txt
-done
