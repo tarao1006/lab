dd369e5e5a57effb64402cb7fc6eda7cd96c5786
tarao1006 <taiga4562@gmail.com>
2020-01-08 14:15:52 +0900

    Add pics and sim01_gather.sh

---
+++ simulation/sim01_gather.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+function ebicp() {
+        scp ebi:/home/katarao/Projects/lab/simulation/$1 $SIM_DIR/$1
+}
+
+ebicp ./sim01/sim01-00/udf/000/output.udf
+ebicp ./sim01/sim01-00/udf/005/output.udf
+ebicp ./sim01/sim01-00/udf/006/output.udf
+ebicp ./sim01/sim01-00/udf/007/output.udf
+ebicp ./sim01/sim01-00/udf/008/output.udf
+ebicp ./sim01/sim01-00/udf/009/output.udf
+ebicp ./sim01/sim01-00/udf/010/output.udf
+ebicp ./sim01/sim01-01/udf/000/output.udf
+ebicp ./sim01/sim01-01/udf/005/output.udf
+ebicp ./sim01/sim01-01/udf/006/output.udf
+ebicp ./sim01/sim01-01/udf/007/output.udf
+ebicp ./sim01/sim01-01/udf/008/output.udf
+ebicp ./sim01/sim01-01/udf/009/output.udf
+ebicp ./sim01/sim01-01/udf/010/output.udf
+ebicp ./sim01/sim01-03/udf/000/output.udf
+ebicp ./sim01/sim01-03/udf/005/output.udf
+ebicp ./sim01/sim01-03/udf/006/output.udf
+ebicp ./sim01/sim01-03/udf/007/output.udf
+ebicp ./sim01/sim01-03/udf/008/output.udf
+ebicp ./sim01/sim01-03/udf/009/output.udf
+ebicp ./sim01/sim01-03/udf/010/output.udf
+ebicp ./sim01/sim01-04/udf/000/output.udf
+ebicp ./sim01/sim01-04/udf/005/output.udf
+ebicp ./sim01/sim01-04/udf/006/output.udf
+ebicp ./sim01/sim01-04/udf/007/output.udf
+ebicp ./sim01/sim01-04/udf/008/output.udf
+ebicp ./sim01/sim01-04/udf/009/output.udf
+ebicp ./sim01/sim01-04/udf/010/output.udf
+ebicp ./sim01/sim01-05/udf/000/output.udf
+ebicp ./sim01/sim01-05/udf/005/output.udf
+ebicp ./sim01/sim01-05/udf/006/output.udf
+ebicp ./sim01/sim01-05/udf/007/output.udf
+ebicp ./sim01/sim01-05/udf/008/output.udf
+ebicp ./sim01/sim01-05/udf/009/output.udf
+ebicp ./sim01/sim01-05/udf/010/output.udf
