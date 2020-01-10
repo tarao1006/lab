415cae14343226bfb11fe7247f251dea9a0a2674
tarao1006 <taiga4562@gmail.com>
2020-01-10 16:02:45 +0900

    Use for loop in gather.sh

--- simulation/sim09/gather.sh
+++ simulation/sim09/gather.sh
@@ -4,13 +4,8 @@ function ebicp() {
         scp ebi:/home/katarao/Projects/lab/simulation/$1 $SIM_DIR/$1
 }
 
-ebicp sim08/udf/00/output.udf
-ebicp sim08/udf/01/output.udf
-ebicp sim08/udf/02/output.udf
-ebicp sim08/udf/03/output.udf
-ebicp sim08/udf/04/output.udf
-ebicp sim08/udf/05/output.udf
-ebicp sim08/udf/06/output.udf
-ebicp sim08/udf/07/output.udf
-ebicp sim08/udf/08/output.udf
-ebicp sim08/udf/09/output.udf
+for i in `seq 0 9`
+do
+        j=`printf %02d $i`
+        ebicp sim09/udf/$j/output.udf
+done
