45ce87498dc87b60eebbbbced9c778e1cad39c9d
tarao1006 <taiga4562@gmail.com>
2020-01-15 15:16:07 +0900

    Remove archive and _archive

--- archive/trim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in `seq 100`
-do
-	num=`printf "%03d" $i`
-	convert $num.jpg -crop 460x460+727+100 _$num.jpg
-done
-ffmpeg -f image2 -r 15 -i _%03d.jpg -r 15 -an -vcodec libx264 -pix_fmt yuv420p video.mp4
