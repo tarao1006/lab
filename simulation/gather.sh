a7f90678e7390c2b3adfa88f963df31aa40c6a1f
tarao1006 <taiga4562@gmail.com>
2020-01-15 15:13:03 +0900

    Add sim12#

--- simulation/gather.sh
+++ simulation/gather.sh
@@ -4,8 +4,15 @@ function ebicp() {
         scp ebi:/home/katarao/Projects/lab/simulation/$1 $SIM_DIR/$1
 }
 
-for i in `seq 1 32`
+for i in `seq 0 9`
 do 
         j=`printf %02d $i`
-        ebicp sim02/sim02-01/udf/$j/output.udf
+        # ebicp sim09/sim09-00/udf/$j/output.udf
+        # ebicp sim10/sim10-00/udf/$j/output.udf
+        # ebicp sim10/sim10-01/udf/$j/output.udf
+        # ebicp sim11/sim11-00/udf/$j/output.udf
+        ebicp sim09/sim09-00/udf/$j/torque_sum.txt
+        # ebicp sim10/sim10-00/udf/$j/torque_sum.txt
+        # ebicp sim10/sim10-01/udf/$j/torque_sum.txt
+        # ebicp sim11/sim11-00/udf/$j/torque_sum.txt
 done
