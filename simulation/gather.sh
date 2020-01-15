bb3e20c8e1546c8d29946b1316b2449136b67957
tarao1006 <taiga4562@gmail.com>
2020-01-15 17:13:26 +0900

    Add sim13

--- simulation/gather.sh
+++ simulation/gather.sh
@@ -7,12 +7,5 @@ function ebicp() {
 for i in `seq 0 9`
 do 
         j=`printf %02d $i`
-        # ebicp sim09/sim09-00/udf/$j/output.udf
-        # ebicp sim10/sim10-00/udf/$j/output.udf
-        # ebicp sim10/sim10-01/udf/$j/output.udf
-        # ebicp sim11/sim11-00/udf/$j/output.udf
-        ebicp sim09/sim09-00/udf/$j/torque_sum.txt
-        # ebicp sim10/sim10-00/udf/$j/torque_sum.txt
-        # ebicp sim10/sim10-01/udf/$j/torque_sum.txt
-        # ebicp sim11/sim11-00/udf/$j/torque_sum.txt
+        ebicp sim12/sim12-00/udf/$j/output.udf
 done
