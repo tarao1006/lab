f53a4689e3b35fe8f659e8a3b885ee03535d14e9
tarao1006 <taiga4562@gmail.com>
2020-01-24 16:55:33 +0900

    Add sim03

--- simulation/sim03/init.py
+++ simulation/sim03/init.py
@@ -1,30 +1,22 @@
 import os
 import shutil
+import numpy as np
 from pathlib import Path
 from pyudf import Udf
 
 udf = Udf()
 udf.load_jsonconfig()
 
-gammadots = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
-B1s = [0.00, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
+udf.data['gravity']['g'] = 0.06
+gammadots = np.arange(0.0, 0.105, 0.005)
 
+if not Path(f'gammadot').exists():
+    os.mkdir(f'gammadot')
 
 for gammadot in gammadots:
-    gammadot_str = f'{gammadot * 100:0=3.0f}'
+    gammadot_str = f'{gammadot * 1000:0=3.0f}'
     udf.data['constitutive_eq']['shear_navier_stokes']['external_field']['dc']['shear_rate'] = gammadot
 
-    if not Path(f'gammadot/{gammadot_str}/B1').exists():
-        os.mkdir(f'gammadot/{gammadot_str}/B1')
-
-    for B1 in B1s:
-        B1_str = f'{B1 * 100:0=3.0f}'
-        if B1_str == '000':
-            udf.data['object_type']['spherical_particle']['particle_spec'][0]['janus_propulsion'] = 'OFF'
-        else:
-            udf.data['object_type']['spherical_particle']['particle_spec'][0]['janus_propulsion'] = 'SQUIRMER'
-            udf.data['object_type']['spherical_particle']['particle_spec'][0]['janus_slip_vel'] = B1
-
-        os.mkdir(f'gammadot/{gammadot_str}/B1/{B1_str}')
-        udf.to_udf(f'gammadot/{gammadot_str}/B1/{B1_str}/input.udf')
-        shutil.copy('/Users/taiga/Projects/lab/simulation/define.udf', f'gammadot/{gammadot_str}/B1/{B1_str}/')
+    os.mkdir(f'gammadot/{gammadot_str}')
+    udf.to_udf(f'gammadot/{gammadot_str}/input.udf')
+    shutil.copy('/Users/taiga/Projects/lab/simulation/define.udf', f'gammadot/{gammadot_str}')
