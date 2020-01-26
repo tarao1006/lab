43d5ed38805909eb7e02fa11a99de8162c61b35d
tarao1006 <taiga4562@gmail.com>
2020-01-26 14:03:53 +0900

    Add sim02/03 results

---
+++ simulation/sim02/gather.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+function ebicp() {
+        scp ebi:/home/katarao/Projects/lab/simulation/$1 $SIM_DIR/$1
+}
+
+for i in `seq 3 3`; do
+    for l in `seq 1 32`; do
+        j=`printf %02d $i`
+        k=`printf %02d $l`
+        ebicp sim02/$j/udf/$k/output.udf
+        ebicp sim02/$j/udf/$k/output.txt
+    done
+done
+
