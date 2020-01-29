61c7a38f5934d742deb1dad1112a628cfeb4908a
tarao1006 <taiga4562@gmail.com>
2020-01-29 14:08:51 +0900

    Add gather.sh

--- simulation/sim03/gather.sh
+++ simulation/sim03/gather.sh
@@ -4,7 +4,7 @@ function ebicp() {
         scp ebi:/home/katarao/Projects/lab/simulation/$1 $SIM_DIR/$1
 }
 
-for i in `seq 0 2`; do
+for i in `seq 2 2`; do
     for l in `seq 0 5 100`; do
         j=`printf %02d $i`
         k=`printf %03d $l`
