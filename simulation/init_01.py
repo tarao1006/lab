2edfb2ec9130bf20ef33d5d58f121e0f4933ba9c
tarao1006 <taiga4562@gmail.com>
2020-01-30 14:09:03 +0900

    Update init_01.py

--- simulation/init_01.py
+++ simulation/init_01.py
@@ -14,13 +14,15 @@ udf.data['switch']['init_distribution']['user_specify']['particles'][0]['q']['q1
 udf.data['switch']['init_distribution']['user_specify']['particles'][0]['q']['q2'] = q.q2
 udf.data['switch']['init_distribution']['user_specify']['particles'][0]['q']['q3'] = q.q3
 
-_shearrates = np.linspace(0.01, 0.1, 10)
+gammadots = np.arange(0.0, 0.105, 0.005)
 
 if not Path('udf').exists():
     os.mkdir('udf')
 
-for i, s in enumerate(_shearrates):
-    udf.data['constitutive_eq']['shear_navier_stokes']['external_field']['dc']['shear_rate'] = s
-    os.mkdir(f'udf/{i:0=2.0f}')
-    udf.to_udf(f'udf/{i:0=2.0f}/input.udf')
-    shutil.copy('/Users/taiga/Projects/lab/simulation/define.udf', f'udf/{i:0=2.0f}/')
+for gammadot in gammadots:
+    gammadot_str = f'{gammadot * 1000:0=3.0f}'
+    udf.data['constitutive_eq']['shear_navier_stokes']['external_field']['dc']['shear_rate'] = gammadot
+
+    os.mkdir(f'udf/{gammadot_str}')
+    udf.to_udf(f'udf/{gammadot_str}/input.udf')
+    shutil.copy('/Users/taiga/Projects/lab/simulation/define.udf', f'udf/{gammadot_str}')
