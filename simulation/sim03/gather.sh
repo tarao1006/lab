3c6f69afcbe29c74151019257d631af66adf0fbd
tarao1006 <taiga4562@gmail.com>
2020-02-13 13:45:32 +0900

    Add some figs

--- simulation/sim03/gather.sh
+++ simulation/sim03/gather.sh
@@ -4,12 +4,12 @@ function ebicp() {
         scp ebi:/home/katarao/Projects/lab/simulation/$1 $SIM_DIR/$1
 }
 
-for i in `seq 3 4`; do
+for i in `seq 0 4`; do
     for l in `seq 0 5 100`; do
         j=`printf %02d $i`
         k=`printf %03d $l`
         ebicp sim03/$j/udf/$k/output.udf
-        ebicp sim03/$j/udf/$k/output.txt
+        # ebicp sim03/$j/udf/$k/output.txt
     done
 done
 
