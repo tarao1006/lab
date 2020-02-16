675f656083c2548317eff9a554da9ab9224e4912
tarao1006 <taiga4562@gmail.com>
2020-02-16 14:53:51 +0900

    Prepare sim05

---
+++ simulation/sim05/gather.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+function ebicp() {
+        scp ebi:/home/katarao/Projects/lab/simulation/$1 $SIM_DIR/$1
+}
+
+for i in `seq 0 0`; do
+    for l in `seq 5 5 150`; do
+        j=`printf %02d $i`
+        k=`printf %03d $l`
+        ebicp sim05/$j/udf/$k/output.udf
+        ebicp sim05/$j/udf/$k/output.txt
+    done
+done
+
