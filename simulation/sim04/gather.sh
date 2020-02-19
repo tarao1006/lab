38134fff261db335abaec095a28c6e2ed89a1286
tarao1006 <taiga4562@gmail.com>
2020-02-19 10:39:18 +0900

    Add sim06

--- simulation/sim04/gather.sh
+++ simulation/sim04/gather.sh
@@ -4,12 +4,12 @@ function ebicp() {
         scp ebi:/home/katarao/Projects/lab/simulation/$1 $SIM_DIR/$1
 }
 
-for i in `seq 0 3`; do
+for i in `seq 1 1`; do
     for l in `seq 5 5 150`; do
         j=`printf %02d $i`
         k=`printf %03d $l`
         ebicp sim04/$j/udf/$k/output.udf
-        ebicp sim04/$j/udf/$k/output.txt
+        # ebicp sim04/$j/udf/$k/output.txt
     done
 done
 
--- simulation/trim.py
+++ simulation/trim.py
@@ -23,14 +23,14 @@ def triming_img(prefix):
     imgs.sort()
     for i, img in tqdm.tqdm(enumerate(imgs)):
         imgarray = cv2.imread(img)
-        cv2.imwrite(f'{prefix}/_{i:0=3.0f}.jpg', crop_center(imgarray, 300, 400))
+        cv2.imwrite(f'{prefix}/_{i:0=3.0f}.jpg', crop_center(imgarray, 250, 350))
 
 
 if __name__ == "__main__":
-    sim_num = 5
-    for i in [2]:  # range(0, 3):
+    sim_num = 6
+    for i in range(0, 3):
         sub_sim_num = i
-        for j in ['030']:  # ['020', '030', '040', '050', '060']:
+        for j in ['020', '030', '040', '050', '060']:
             prefix = f'/Users/taiga/Projects/lab/simulation/sim{sim_num:0=2.0f}/{sub_sim_num:0=2.0f}/udf/{j}/output/output.udf/graphic'
             triming_img(prefix)
         # subprocess.run(['ffmpeg', '-f', 'image2', '-r', '15', '-i', f'{prefix}/_%03d.jpg', '-r', '15', '-an', '-vcodec', 'libx264', '-pix_fmt', 'yuv420p', f'{sim_num:0=2.0f}_{sub_sim_num:0=3.0f}_video.mp4'])
